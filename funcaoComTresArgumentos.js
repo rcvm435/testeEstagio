eda0009d0965f7c371bdddaa17ce00c337689155
Rafael Castro <jotag3x@gmail.com>
2023-01-11 21:55:29 -0300

    q1/q2/q3/q4/q5

---
+++ funcaoComTresArgumentos.js
@@ -0,0 +1,17 @@
+const callable = (str) => {
+    vowels = ['a', 'e', 'i', 'o', 'u'];
+    let stringToReturn = '';
+    for (let i = 0; i < str.length; i++) {
+      stringToReturn += vowels.includes(str[i].toLowerCase()) ? 0 : str[i];
+    }
+  
+    return stringToReturn;
+  };
+ 
+  const q2 = (callable, str1, str2) => {
+    str1 = callable(str1);
+    str2 = callable(str2);
+  
+    console.log(`String1: ${str1} | String2: ${str2}`);
+  };
+  
