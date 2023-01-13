01df9213fec443e9a2050f145777e92ec1204b57
rcvm435 <109398542+rcvm435@users.noreply.github.com>
2023-01-13 12:45:19 -0300

    Update q4.js

---
+++ q4.js
@@ -0,0 +1,10 @@
+function substtituindoOcorrencias(){
+    const q4 = () => {
+        const text = 'teste 1 de 2 string 3';
+      
+        replacedText = text.replace(new RegExp('[0-9]', 'g'), '[removido]');
+      
+        console.log(replacedText);
+
+}
+}
