88cd382fc22f2e57853e9894469a454b27434d9d
rcvm435 <109398542+rcvm435@users.noreply.github.com>
2023-01-13 12:43:30 -0300

    Update q1.js

---
+++ q1.js
@@ -0,0 +1,8 @@
+function maiorEMenor(stringA, stringB) {
+   
+    let maiorStr;
+    if (stringA.length > stringB.length) {
+        return maiorStr = stringA;
+    } else return  maiorStr= stringB;
+}
+console.log(maiorEMenor("batata", "tomate"))
